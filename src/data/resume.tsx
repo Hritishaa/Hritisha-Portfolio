792175fa1d1b8a737092428e0c02bee0d05d0d84
Hritisha Sahota <143306447+Hritishaa@users.noreply.github.com>
2024-12-09 18:07:20 +0530

    Changes in images and experience

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,9 +10,10 @@ export const DATA = {
   description:
     "Creative mind with a passion for building connections and solving problems. DSA enthusiast, always exploring new ways to innovate and inspire!",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "Placement Coordinator for my batch at Bennett University, where I’m a third-year CSE student. I love connecting with new people and building meaningful relationships. A passionate dancer and piano enthusiast, I find joy in rhythm and melodies. Cooking is my creative escape, where I whip up delicious dishes. Balancing academics and hobbies, I’m always looking for ways to grow and inspire!",
   avatarUrl: "/pro.jpeg",
   skills: [
+    "DSA",
     "React",
     "Next.js",
     "Typescript",
@@ -20,7 +21,6 @@ export const DATA = {
     "Python",
     "Go",
     "Docker",
-
     "Java",
     "C++",
   ],
@@ -71,76 +71,40 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
+      company: "Placement Committee",
+      href: "https://www.linkedin.com/company/career-services-center-bennett-university/mycompany/",
       badges: [],
+      location: "Greater Noida",
+      title: "Co-lead",
+      logoUrl: "/pro4.png",
+      start: "August 2023",
+      end: "Current",
+      description:
+        "As the Co-Lead of the Placement Committee at Bennett University, I play a key role in connecting students with recruiters. I manage placement drives, coordinate with companies to create hiring opportunities, and ensure smooth communication between students and recruiters. I also guide my peers in interview preparation and help organize training sessions to enhance their employability skills.",
+    },
+    {
+      company: "Skillarena",
+      badges: [],
+      href: "https://skillarena.in",
       location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
-      start: "May 2021",
-      end: "Oct 2022",
+      title: "Technical Sales",
+      logoUrl: "/pro5.png",
+      start: "June 2024",
+      end: "July 2024",
       description:
-        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+        "As a Technical Sales Intern at Skillarena, I focused on bridging the gap between technology and client needs. My role involved understanding client requirements, presenting tailored technical solutions, and effectively demonstrating the value of Skillarena's products and services. I also collaborated with the sales and technical teams to strategize and drive conversions, honing my skills in communication, problem-solving, and client relationship management.",
     },
     {
-      company: "Shopify",
+      company: "Student Cabinet",
+      href: "https://www.linkedin.com/company/scset-student-cabinet-bennett-university/?originalSubdomain=in",
       badges: [],
-      href: "https://shopify.com",
-      location: "Remote",
-      title: "Software Engineer",
-      logoUrl: "/shopify.svg",
-      start: "January 2021",
-      end: "April 2021",
+      location: "Greater Noida, India",
+      title: "Minister of Alumni Relations",
+      logoUrl: "/pro6.png",
+      start: "September 2023",
+      end: "May 2024",
       description:
-        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
-    },
-    {
-      company: "Nvidia",
-      href: "https://nvidia.com/",
-      badges: [],
-      location: "Santa Clara, CA",
-      title: "Software Engineer",
-      logoUrl: "/nvidia.png",
-      start: "January 2020",
-      end: "April 2020",
-      description:
-        "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
-    },
-    {
-      company: "Splunk",
-      href: "https://splunk.com",
-      badges: [],
-      location: "San Jose, CA",
-      title: "Software Engineer",
-      logoUrl: "/splunk.svg",
-      start: "January 2019",
-      end: "April 2019",
-      description:
-        "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
-    },
-    {
-      company: "Lime",
-      href: "https://li.me/",
-      badges: [],
-      location: "San Francisco, CA",
-      title: "Software Engineer",
-      logoUrl: "/lime.svg",
-      start: "January 2018",
-      end: "April 2018",
-      description:
-        "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
-    },
-    {
-      company: "Mitre Media",
-      href: "https://mitremedia.com/",
-      badges: [],
-      location: "Toronto, ON",
-      title: "Software Engineer",
-      logoUrl: "/mitremedia.png",
-      start: "May 2017",
-      end: "August 2017",
-      description:
-        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+        "As the Minister of Alumni Relations in the Student Cabinet at Bennett University, I served as a bridge between the alumni community and current students. My role involved organizing alumni engagement programs, networking events, and mentorship opportunities to foster meaningful connections. I collaborated with alumni to bring valuable insights and experiences to students while ensuring the alumni remained closely connected to the university. Through this position, I enhanced my leadership, event management, and communication skills.",
     },
   ],
   education: [
